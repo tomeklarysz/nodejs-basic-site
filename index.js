b923ae1065b8e0fa0e1159f41c1337782a5ac924
= <ttomek.larysz@gmail.com>
2025-03-03 21:19:13 +0100

    express rewrite

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-const http = require('http');
+// const http = require('http');
 const fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.createServer((req, res) => {
-  const { url } = req;
-  filename = '404.html';
-  switch (url) {
-    case '/':
-      filename = 'index.html';
-      break;
-    case '/about':
-      filename = 'about.html';
-      break;
-    case '/contact-me':
-      filename = 'contact-me.html';
-      break;
-  }
-  fs.readFile(filename, (err, data) => {
-    if (err) throw err;
-    if (filename === '/home/tomek/learn-backend/basic-site/404.html') {
-      res.writeHead(400, {'Content-Type': 'text/html'});
-    }
-    else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-    } 
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
+app.use(express.static('public'))
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`);
+});
+
+// http.createServer((req, res) => {
+//   const { url } = req;
+//   filename = '404.html';
+//   switch (url) {
+//     case '/':
+//       filename = 'index.html';
+//       break;
+//     case '/about':
+//       filename = 'about.html';
+//       break;
+//     case '/contact-me':
+//       filename = 'contact-me.html';
+//       break;
+//   }
+//   fs.readFile(filename, (err, data) => {
+//     if (err) throw err;
+//     if (filename === '/home/tomek/learn-backend/basic-site/404.html') {
+//       res.writeHead(400, {'Content-Type': 'text/html'});
+//     }
+//     else {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//     } 
+//     res.write(data);
+//     res.end();
+//   });
+// }).listen(8080);
